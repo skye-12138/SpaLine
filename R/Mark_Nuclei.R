978c13fecf196aa70077cb269e6d66df67f6c1c7
skye-12138 <wurenyan@outlook.com>
2023-03-29 23:52:49 +0800

    python

--- R/Mark_Nuclei.R
+++ R/Mark_Nuclei.R
@@ -10,6 +10,14 @@
 #'
 
 Mark_Nuclei<-function(path,QC_dir=NULL,project){
+  env<-reticulate::conda_list()
+  if("SpaLine_conda" %in% env$name){
+    reticulate::use_condaenv("SpaLine_conda", required = TRUE)
+  }else{
+    reticulate::conda_create("SpaLine_conda")
+    reticulate::use_condaenv("SpaLine_conda", required = TRUE)
+  }
+  reticulate::py_config()
   reticulate::py_run_file(system.file("python","st_nucleus.py",package = "SpaLine"))
   reticulate::py_run_file(system.file("python","st_segment.py",package = "SpaLine"))
   adata <- feature_tiling(inDir=path,outDir=QC_dir,project=project)
--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,3 @@
 .onAttach<-function(libname,pkgname){
   reticulate::configure_environment(pkgname)
-  env<-reticulate::conda_list()
-  if("SpaLine_conda" %in% env$name){
-    reticulate::use_condaenv("SpaLine_conda", required = TRUE)
-  }else{
-    reticulate::conda_create("SpaLine_conda")
-    reticulate::use_condaenv("SpaLine_conda", required = TRUE)
-  }
-  reticulate::py_config()
 }
