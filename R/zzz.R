48b0e69f970447a3de43f5a3be50e8f49f48239c
skye-12138 <wurenyan@outlook.com>
2023-03-29 19:12:24 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onAttach<-function(libname,pkgname){
   reticulate::configure_environment(pkgname)
+  reticulate::py_config()
   reticulate::py_run_file(system.file("python","st_nucleus.py",package = "SpaLine"))
   reticulate::py_run_file(system.file("python","st_segment.py",package = "SpaLine"))
 }
