20f90825441ac28edb0806223a6fa05395e7f70f
skye-12138 <wurenyan@outlook.com>
2023-05-11 14:50:12 +0800

    reticulate update

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
 .onAttach<-function(libname,pkgname){
   reticulate::configure_environment(pkgname)
+  reticulate::conda_create("SpaLine_conda")
+  reticulate::use_condaenv("SpaLine_conda", required = TRUE)
+  reticulate::py_config()
+  reticulate::py_run_file(system.file("python","st_nucleus.py",package = "SpaLine"))
+  reticulate::py_run_file(system.file("python","st_segment.py",package = "SpaLine"))
 }
